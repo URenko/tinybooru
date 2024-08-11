279e4baec5bfa26ffd8f1936e1ef91a245a365e4
URenko <18209292+URenko@users.noreply.github.com>
2024-08-11 20:47:33 +0000

    [thumb] switch to bilibili

--- thumb_gen.py
+++ thumb_gen.py
@@ -3,7 +3,7 @@ from rich.traceback import install
 install()
 from rich import print
 
-import sqlite3, io, http, time, tempfile, subprocess
+import sqlite3, io, http, time, tempfile, subprocess, base64, mimetypes, os
 from pathlib import Path, PurePosixPath
 from urllib.parse import quote_plus
 from PIL import Image, UnidentifiedImageError
@@ -15,6 +15,7 @@ s = requests.Session()
 _retry = Retry(backoff_factor=1, status_forcelist=[502,], allowed_methods=None)
 s.mount('http://', HTTPAdapter(max_retries=_retry))
 s.mount('https://', HTTPAdapter(max_retries=_retry))
+s.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:130.0) Gecko/20100101 Firefox/130.0"})
 
 
 from config import local_root
@@ -42,19 +43,30 @@ def update_thumb(source: str, db: sqlite3.Connection, target_size=5*2**20):
             thumb_file = open(origin_path, 'rb')
             filename = origin_name
         else:
+            if im.mode == 'RGBA':  # cannot write mode RGBA as JPEG
+                im = im.convert('RGB')
             reduce_factor = 0
-            thumb_size = target_size # force start
+            thumb_size = target_size # force startj
             while thumb_size >= target_size:
                 reduce_factor += 1
                 im_tmp = im.reduce(reduce_factor) if reduce_factor != 1 else im
                 thumb_file = io.BytesIO()
-                im_tmp.save(thumb_file, 'WEBP', method=6)
+                im_tmp.save(thumb_file, 'JPEG', optimize=True)#'WEBP', method=6)
                 thumb_size = len(thumb_file.getvalue())
                 print(f"{origin_size/2**10:.1f} KB reduced {reduce_factor} times to {thumb_size/2**10:.1f} KB")
             thumb_file.seek(0)
-            filename = str(Path(origin_name).with_suffix('.webp'))
+            filename = str(Path(origin_name).with_suffix('.jpg'))
     
-    r = s.post("https://p.sda1.dev/api_dup2/v1/upload_external_noform?filename="+quote_plus(filename), data=thumb_file)
+    # r = s.post("https://p.sda1.dev/api_dup2/v1/upload_external_noform?filename="+quote_plus(filename), data=thumb_file)
+    r = s.post(
+        "https://member.bilibili.com/x/vu/web/cover/up",
+        data={
+            'csrf': os.environ['csrf'],
+            'cover': b'data:' + mimetypes.guess_type(filename, strict=False)[0].encode('UTF-8') + b';base64,' + base64.b64encode(thumb_file.read()),
+        },
+        cookies={'SESSDATA': os.environ['SESSDATA']}
+    )
+    # https://github.com/SocialSisterYi/bilibili-API-collect/pull/1066/files#diff-36446fff007e20642f1fe9dd135046f63817bfa63a6589426821a9f421b558f5
     
     if _jxl: _jpg_tmp_file.close()
 
@@ -63,8 +75,12 @@ def update_thumb(source: str, db: sqlite3.Connection, target_size=5*2**20):
         raise RuntimeError
     try:
         j = r.json()
+        assert j['code'] == 0, r.text
         print(j['message'])
         URL = j['data']['url']
+        if URL.startswith('http://'):
+            URL = 'https://' + URL[:len('http://')]
+        URL += '@progressive'
     except KeyError:
         print(r.text)
         raise

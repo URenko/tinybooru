c47287d9adf9963fe0c1512f35e95d1426e60ef3
URenko <18209292+URenko@users.noreply.github.com>
2024-09-22 16:11:50 +0000

    [img_like] update threshold

--- img_like.py
+++ img_like.py
@@ -16,9 +16,9 @@ def ML_sim(a, b, comment=''):
     fa = m(stack)
     fa = F.normalize(fa, p=2, dim=1) 
     similarity = fa[0].dot(fa[1]).item()
-    if similarity > 0.7117 and similarity <= 0.7166:
+    if similarity > 0.7117 and similarity <= 0.7136:
         input(f"{comment}\n{similarity=}, 可疑")
-    return similarity > 0.7166
+    return similarity > 0.7136
  
 # threshold = 0.7
 # 0.8985 0.8639 0.8199 0.7863
